9a7959ff4c23d0ab572344729c81a47fdbfb2ee2
Kresten Krab Thorup <krab@trifork.com>
2012-01-23 10:41:03 +0100

    Simplify KEY_IN_RANGE macro

--- src/lsm_btree.hrl
+++ src/lsm_btree.hrl
@@ -46,14 +46,4 @@
              (Key <  Range#btree_range.to_key)))).
 
 -define(KEY_IN_RANGE(Key,Range),
-        ((Range#btree_range.from_inclusive andalso
-          (Range#btree_range.from_key =< Key))
-         orelse
-           (Range#btree_range.from_key < Key))
-        andalso
-        ((Range#btree_range.to_key == undefined)
-         orelse
-         ((Range#btree_range.to_inclusive andalso
-             (Key =< Range#btree_range.to_key))
-          orelse
-             (Key <  Range#btree_range.to_key)))).
+        (?KEY_IN_FROM_RANGE(Key,Range) andalso ?KEY_IN_TO_RANGE(Key,Range))).
