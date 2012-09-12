krestenkrab/hanoidb

commit f324972cdd91e6f457d0a4dd11beac1a80332e7e

Cleanup compiler warnings and remove (for now) the predictable strategy as it has some issues to be worked out.

src/hanoidb_level.erl
@@ -61,7 +61,6 @@
 %% merge, just to clean up and optimize future operations.
 -define(MERGE_TIMEOUT,1000).
 
-
 -ifdef(DEBUG).
 -define(log(Fmt,Args), debug_log(State,Fmt,Args)).
 debug_log(State,Fmt,Args) ->
@@ -169,17 +168,17 @@ initialize2(State) ->
     MFileName = filename("M",State),
 
     %% remove old merge file
-    file:delete( filename("X",State)),
+    file:delete(filename("X",State)),
 
     %% remove old fold files (hard links to A/B/C used during fold)
-    file:delete( filename("AF",State)),
-    file:delete( filename("BF",State)),
-    file:delete( filename("CF",State)),
+    file:delete(filename("AF",State)),
+    file:delete(filename("BF",State)),
+    file:delete(filename("CF",State)),
 
     case file:read_file_info(MFileName) of
         {ok, _} ->
 
-            %% recover from post-merge crash. This is the case where
+            %% Recover from post-merge crash. This is the case where
             %% a merge completed, resulting in a file that needs to
             %% stay at the *same* level because the resulting size
             %% is smaller than or equal to this level's files.
@@ -187,32 +186,34 @@ initialize2(State) ->
             file:delete(BFileName),
             ok = file:rename(MFileName, AFileName),
 
-            {ok, BTA} = hanoidb_reader:open(AFileName, [random|State#state.opts]),
+            {ok, IXA} = hanoidb_reader:open(AFileName, [random|State#state.opts]),
 
             case file:read_file_info(CFileName) of
                 {ok, _} ->
                     file:rename(CFileName, BFileName),
-                    {ok, BTB} = hanoidb_reader:open(BFileName, [random|State#state.opts]),
-                    check_begin_merge_then_loop0(init_state(State#state{ a= BTA, b=BTB }));
+                    {ok, IXB} = hanoidb_reader:open(BFileName, [random|State#state.opts]),
+                    check_begin_merge_then_loop0(init_state(State#state{ a= IXA, b=IXB }));
 
                 {error, enoent} ->
-                    main_loop(init_state(State#state{ a= BTA, b=undefined }))
+                    main_loop(init_state(State#state{ a= IXA, b=undefined }))
             end;
 
         {error, enoent} ->
             case file:read_file_info(BFileName) of
                 {ok, _} ->
-                    {ok, BTA} = hanoidb_reader:open(AFileName, [random|State#state.opts]),
-                    {ok, BTB} = hanoidb_reader:open(BFileName, [random|State#state.opts]),
+                    {ok, IXA} = hanoidb_reader:open(AFileName, [random|State#state.opts]),
+                    {ok, IXB} = hanoidb_reader:open(BFileName, [random|State#state.opts]),
 
-                    case file:read_file_info(CFileName) of
-                        {ok, _} ->
-                            {ok, BTC} = hanoidb_reader:open(CFileName, [random|State#state.opts]);
+                    IXC =
+                        case file:read_file_info(CFileName) of
+                            {ok, _} ->
+                                {ok, C} = hanoidb_reader:open(CFileName, [random|State#state.opts]),
+                                C;
                         {error, enoent} ->
-                            BTC = undefined
+                            undefined
                     end,
 
-                    check_begin_merge_then_loop0(init_state(State#state{ a=BTA, b=BTB, c=BTC }));
+                    check_begin_merge_then_loop0(init_state(State#state{ a=IXA, b=IXB, c=IXC }));
 
                 {error, enoent} ->
 
@@ -221,8 +222,8 @@ initialize2(State) ->
 
                     case file:read_file_info(AFileName) of
                         {ok, _} ->
-                            {ok, BTA} = hanoidb_reader:open(AFileName, [random|State#state.opts]),
-                            main_loop(init_state(State#state{ a=BTA }));
+                            {ok, IXA} = hanoidb_reader:open(AFileName, [random|State#state.opts]),
+                            main_loop(init_state(State#state{ a=IXA }));
 
                         {error, enoent} ->
                             main_loop(init_state(State))
@@ -234,15 +235,16 @@ init_state(State) ->
     ?log("opened level ~p, state=~p", [State#state.level, State]),
     State.
 
-check_begin_merge_then_loop0(State=#state{a=BTA, b=BTB, merge_pid=undefined})
-  when BTA/=undefined, BTB /= undefined ->
+check_begin_merge_then_loop0(State=#state{a=IXA, b=IXB, merge_pid=undefined})
+  when IXA/=undefined, IXB /= undefined ->
     {ok, MergePID} = begin_merge(State),
     MergeRef = monitor(process, MergePID),
-    if State#state.c == undefined ->
-            WIP = ?BTREE_SIZE(State#state.level);
-       true ->
-            WIP = 2*?BTREE_SIZE(State#state.level)
-    end,
+    WIP =
+        if State#state.c == undefined ->
+                ?BTREE_SIZE(State#state.level);
+           true ->
+                2*?BTREE_SIZE(State#state.level)
+        end,
 
     MergePID ! {step, {self(), MergeRef}, WIP},
     main_loop(State#state{merge_pid=MergePID,work_done=0,
@@ -252,8 +254,8 @@ check_begin_merge_then_loop0(State) ->
     check_begin_merge_then_loop(State).
 
 
-check_begin_merge_then_loop(State=#state{a=BTA, b=BTB, merge_pid=undefined})
-  when BTA/=undefined, BTB /= undefined ->
+check_begin_merge_then_loop(State=#state{a=IXA, b=IXB, merge_pid=undefined})
+  when IXA/=undefined, IXB /= undefined ->
     {ok, MergePID} = begin_merge(State),
     main_loop(State#state{merge_pid=MergePID,work_done=0 });
 check_begin_merge_then_loop(State) ->
@@ -286,17 +288,15 @@ main_loop(State = #state{ next=Next }) ->
 
         ?CALL(From, {inject, FileName}) when State#state.c == undefined ->
 
-            case {State#state.a, State#state.b} of
-                {undefined, undefined} ->
-                    ToFileName = filename("A",State),
-                    SetPos = #state.a;
-                {_, undefined} ->
-                    ToFileName = filename("B",State),
-                    SetPos = #state.b;
-                {_, _} ->
-                    ToFileName = filename("C",State),
-                    SetPos = #state.c
-            end,
+            {ToFileName, SetPos} =
+                case {State#state.a, State#state.b} of
+                    {undefined, undefined} ->
+                        {filename("A",State), #state.a};
+                    {_, undefined} ->
+                        {filename("B",State), #state.b};
+                    {_, _} ->
+                        {filename("C",State), #state.c}
+                end,
 
             ?log("inject ~s~n", [ToFileName]),
 
@@ -362,12 +362,13 @@ main_loop(State = #state{ next=Next }) ->
             State1 = State#state{ work_done = State#state.work_done + State#state.work_in_progress,
                                   work_in_progress = 0 },
 
-            case State1#state.step_next_ref of
-                undefined ->
-                    State2 = reply_step_ok(State1);
-                _ ->
-                    State2 = State1
-            end,
+            State2 =
+                case State1#state.step_next_ref of
+                    undefined ->
+                        reply_step_ok(State1);
+                    _ ->
+                        State1
+                end,
 
             main_loop(State2#state{ step_merge_ref=undefined });
 
@@ -380,12 +381,13 @@ main_loop(State = #state{ next=Next }) ->
             ?log("merge worker died ~p", [_Reason]),
             %% current merge worker died (or just finished)
 
-            case State#state.step_next_ref of
-                undefined ->
-                    State2 = reply_step_ok(State);
-                _ ->
-                    State2 = State
-            end,
+            State2 =
+                case State#state.step_next_ref of
+                    undefined ->
+                        reply_step_ok(State);
+                    _ ->
+                        State
+                end,
 
             main_loop(State2#state{ step_merge_ref=undefined, work_in_progress=0 });
 
@@ -437,16 +439,15 @@ main_loop(State = #state{ next=Next }) ->
 
             ?log("init_range_fold ~p -> ~p", [Range, WorkerPID]),
 
+            {FoldingPIDs, NextList} =
             case {State#state.a, State#state.b, State#state.c} of
                 {undefined, undefined, undefined} ->
-                    FoldingPIDs = [],
-                    NextList = List;
+                    {[], List};
 
                 {_, undefined, undefined} ->
                     ok = file:make_link(filename("A", State), filename("AF", State)),
                     {ok, PID0} = start_range_fold(filename("AF",State), WorkerPID, Range, State),
-                    NextList = [PID0|List],
-                    FoldingPIDs = [PID0];
+                    {[PID0|List], [PID0]};
 
                 {_, _, undefined} ->
                     ok = file:make_link(filename("A", State), filename("AF", State)),
@@ -455,8 +456,7 @@ main_loop(State = #state{ next=Next }) ->
                     ok = file:make_link(filename("B", State), filename("BF", State)),
                     {ok, PIDB} = start_range_fold(filename("BF",State), WorkerPID, Range, State),
 
-                    NextList = [PIDA,PIDB|List],
-                    FoldingPIDs = [PIDB,PIDA];
+                    {[PIDA,PIDB|List], [PIDB,PIDA]};
 
                 {_, _, _} ->
                     ok = file:make_link(filename("A", State), filename("AF", State)),
@@ -468,8 +468,7 @@ main_loop(State = #state{ next=Next }) ->
                     ok = file:make_link(filename("C", State), filename("CF", State)),
                     {ok, PIDC} = start_range_fold(filename("CF",State), WorkerPID, Range, State),
 
-                    NextList = [PIDA,PIDB,PIDC|List],
-                    FoldingPIDs = [PIDC,PIDB,PIDA]
+                    {[PIDA,PIDB,PIDC|List], [PIDC,PIDB,PIDA]}
             end,
 
             case Next of
@@ -488,36 +487,37 @@ main_loop(State = #state{ next=Next }) ->
 
         ?CALL(From, {init_blocking_range_fold, WorkerPID, Range, List}) ->
 
-            case {State#state.a, State#state.b, State#state.c} of
-                {undefined, undefined, undefined} ->
-                    RefList = List;
+            RefList =
+                case {State#state.a, State#state.b, State#state.c} of
+                    {undefined, undefined, undefined} ->
+                        List;
 
-                {_, undefined, undefined} ->
-                    ARef = erlang:make_ref(),
-                    ok = do_range_fold(State#state.a, WorkerPID, ARef, Range),
-                    RefList = [ARef|List];
+                    {_, undefined, undefined} ->
+                        ARef = erlang:make_ref(),
+                        ok = do_range_fold(State#state.a, WorkerPID, ARef, Range),
+                        [ARef|List];
 
-                {_, _, undefined} ->
-                    BRef = erlang:make_ref(),
-                    ok = do_range_fold(State#state.b, WorkerPID, BRef, Range),
+                    {_, _, undefined} ->
+                        BRef = erlang:make_ref(),
+                        ok = do_range_fold(State#state.b, WorkerPID, BRef, Range),
 
-                    ARef = erlang:make_ref(),
-                    ok = do_range_fold(State#state.a, WorkerPID, ARef, Range),
+                        ARef = erlang:make_ref(),
+                        ok = do_range_fold(State#state.a, WorkerPID, ARef, Range),
 
-                    RefList = [ARef,BRef|List];
+                        [ARef,BRef|List];
 
-                {_, _, _} ->
-                    CRef = erlang:make_ref(),
-                    ok = do_range_fold(State#state.c, WorkerPID, CRef, Range),
+                    {_, _, _} ->
+                        CRef = erlang:make_ref(),
+                        ok = do_range_fold(State#state.c, WorkerPID, CRef, Range),
 
-                    BRef = erlang:make_ref(),
-                    ok = do_range_fold(State#state.b, WorkerPID, BRef, Range),
+                        BRef = erlang:make_ref(),
+                        ok = do_range_fold(State#state.b, WorkerPID, BRef, Range),
 
-                    ARef = erlang:make_ref(),
-                    ok = do_range_fold(State#state.a, WorkerPID, ARef, Range),
+                        ARef = erlang:make_ref(),
+                        ok = do_range_fold(State#state.a, WorkerPID, ARef, Range),
 
-                    RefList = [ARef,BRef,CRef|List]
-            end,
+                        [ARef,BRef,CRef|List]
+                end,
 
             case Next of
                 undefined ->
@@ -606,14 +606,15 @@ main_loop(State = #state{ next=Next }) ->
             {ok, State2} = close_and_delete_a_and_b(State),
 
             % if there is a "C" file, then move it to "A" position.
-            case State2#state.c of
-                undefined ->
-                    State3=State2;
-                TreeFile ->
-                    %% TODO: on what OS's is it ok to rename an open file?
-                    ok = file:rename(filename("C", State2), filename("A", State2)),
-                    State3 = State2#state{ a = TreeFile, b=undefined, c=undefined }
-            end,
+            State3 =
+                case State2#state.c of
+                    undefined ->
+                        State2;
+                    TreeFile ->
+                        %% TODO: on what OS's is it ok to rename an open file?
+                        ok = file:rename(filename("C", State2), filename("A", State2)),
+                        State2#state{ a = TreeFile, b=undefined, c=undefined }
+                end,
 
             main_loop(State3#state{ inject_done_ref=undefined });
 
@@ -646,46 +647,58 @@ main_loop(State = #state{ next=Next }) ->
             ?log("got unexpected exit ~p from ~p~n", [Reason, PID]),
             error_logger:info_msg("got unexpected exit ~p from ~p~n", [Reason, PID])
 
-
     end.
 
 do_step(StepFrom, PreviousWork, StepSize, State) ->
-    if (State#state.b =/= undefined) andalso (State#state.merge_pid =/= undefined) ->
-            WorkLeftHere = max(0, (2 * ?BTREE_SIZE(State#state.level)) - State#state.work_done);
-       true ->
-            WorkLeftHere = 0
-    end,
+    WorkLeftHere =
+        if (State#state.b =/= undefined) andalso (State#state.merge_pid =/= undefined) ->
+                max(0, (2 * ?BTREE_SIZE(State#state.level)) - State#state.work_done);
+           true ->
+                 0
+        end,
     WorkUnit      = StepSize,
     MaxLevel      = max(State#state.max_level, State#state.level),
-    TotalWork     = (MaxLevel-?TOP_LEVEL+1) * WorkUnit,
+    Depth         = MaxLevel-?TOP_LEVEL+1,
+    TotalWork     = Depth * WorkUnit,
     WorkUnitsLeft = max(0, TotalWork-PreviousWork),
 
-    case hanoidb:get_opt( merge_strategy, State#state.opts, fast) of
-        fast ->
-            WorkToDoHere = min(WorkLeftHere, WorkUnitsLeft);
-        predictable ->
-            WorkToDoHere = min(WorkLeftHere, WorkUnit)
-    end,
-
+    %% TODO: The goal was to offer a less variable latency option for merging but this
+    %% heuristic doesn't work when there are aggressive deletes (expiry or delete).
+    %% https://github.com/basho/hanoidb/issues/7
+    WorkToDoHere =
+        min(WorkLeftHere, WorkUnitsLeft),
+        %% case hanoidb:get_opt( merge_strategy, State#state.opts, fast) of
+        %%     fast ->
+        %%         min(WorkLeftHere, WorkUnitsLeft);
+        %%     predictable ->
+        %%         if (WorkLeftHere < Depth * WorkUnit) ->
+        %%                 min(WorkLeftHere, WorkUnit);
+        %%            true ->
+        %%                 min(WorkLeftHere, WorkUnitsLeft)
+        %%         end
+        %% end,
     WorkIncludingHere  = PreviousWork + WorkToDoHere,
 
     ?log("do_step prev:~p, do:~p of ~p ~n", [PreviousWork, WorkToDoHere, WorkLeftHere]),
 
     %% delegate the step_level request to the next level
     Next = State#state.next,
-    if Next =:= undefined ->
-            DelegateRef = undefined;
-       true ->
-            DelegateRef = plain_rpc:send_call(Next, {step_level, WorkIncludingHere, StepSize})
+    DelegateRef =
+        if Next =:= undefined ->
+                undefined;
+           true ->
+                plain_rpc:send_call(Next, {step_level, WorkIncludingHere, StepSize})
     end,
 
-    if WorkToDoHere > 0 ->
-            MergePID = State#state.merge_pid,
-            MergeRef = monitor(process, MergePID),
-            MergePID ! {step, {self(), MergeRef}, WorkToDoHere};
-       true ->
-            MergeRef = undefined
-    end,
+    MergeRef =
+        if WorkToDoHere > 0 ->
+                MergePID = State#state.merge_pid,
+                Monitor = monitor(process, MergePID),
+                MergePID ! {step, {self(), Monitor}, WorkToDoHere},
+                Monitor;
+           true ->
+                undefined
+        end,
 
     if (DelegateRef =:= undefined) andalso (MergeRef =:= undefined) ->
             %% nothing to do ... just return OK

src/hanoidb_merger.erl
@@ -26,7 +26,7 @@
 -author('Kresten Krab Thorup <krab@trifork.com>').
 -author('Gregory Burd <greg@burd.me>').
 
-%% @doc Merging two BTrees
+%% @doc Merging two Indexes
 
 -export([merge/6]).
 
@@ -41,8 +41,8 @@
 -define(LOCAL_WRITER, true).
 
 merge(A,B,C, Size, IsLastLevel, Options) ->
-    {ok, BT1} = hanoidb_reader:open(A, [sequential|Options]),
-    {ok, BT2} = hanoidb_reader:open(B, [sequential|Options]),
+    {ok, IXA} = hanoidb_reader:open(A, [sequential|Options]),
+    {ok, IXB} = hanoidb_reader:open(B, [sequential|Options]),
     {ok, Out} =
         case ?LOCAL_WRITER of
             true ->
@@ -51,16 +51,16 @@ merge(A,B,C, Size, IsLastLevel, Options) ->
                 hanoidb_writer:open(C, [{size, Size} | Options])
         end,
     AKVs =
-        case hanoidb_reader:first_node(BT1) of
+        case hanoidb_reader:first_node(IXA) of
             {node, AKV} -> AKV;
             none -> []
         end,
     BKVs =
-        case hanoidb_reader:first_node(BT2) of
+        case hanoidb_reader:first_node(IXB) of
             {node, BKV} ->BKV;
             none -> []
         end,
-    scan(BT1, BT2, Out, IsLastLevel, AKVs, BKVs, {0, none}).
+    scan(IXA, IXB, Out, IsLastLevel, AKVs, BKVs, {0, none}).
 
 terminate(Out) ->
     case ?LOCAL_WRITER of
@@ -84,14 +84,14 @@ hibernate_scan(Keep) ->
     erlang:garbage_collect(),
     receive
         {step, From, HowMany} ->
-            {BT1, BT2, Out, IsLastLevel, AKVs, BKVs, N} = erlang:binary_to_term(zlib:gunzip(Keep)),
-            scan(hanoidb_reader:deserialize(BT1),
-                 hanoidb_reader:deserialize(BT2),
+            {IXA, IXB, Out, IsLastLevel, AKVs, BKVs, N} = erlang:binary_to_term(zlib:gunzip(Keep)),
+            scan(hanoidb_reader:deserialize(IXA),
+                 hanoidb_reader:deserialize(IXB),
                  hanoidb_writer:deserialize(Out),
                  IsLastLevel, AKVs, BKVs, {N+HowMany, From})
     end.
 
-scan(BT1, BT2, Out, IsLastLevel, AKVs, BKVs, {N, FromPID}) when N < 1, AKVs =/= [], BKVs =/= [] ->
+scan(IXA, IXB, Out, IsLastLevel, AKVs, BKVs, {N, FromPID}) when N < 1, AKVs =/= [], BKVs =/= [] ->
     case FromPID of
         none ->
             ok;
@@ -101,39 +101,39 @@ scan(BT1, BT2, Out, IsLastLevel, AKVs, BKVs, {N, FromPID}) when N < 1, AKVs =/= 
 
     receive
         {step, From, HowMany} ->
-            scan(BT1, BT2, Out, IsLastLevel, AKVs, BKVs, {N+HowMany, From})
+            scan(IXA, IXB, Out, IsLastLevel, AKVs, BKVs, {N+HowMany, From})
     after ?HIBERNATE_TIMEOUT ->
             case ?LOCAL_WRITER of
                 true ->
-                    Args = {hanoidb_reader:serialize(BT1),
-                            hanoidb_reader:serialize(BT2),
+                    Args = {hanoidb_reader:serialize(IXA),
+                            hanoidb_reader:serialize(IXB),
                             hanoidb_writer:serialize(Out), IsLastLevel, AKVs, BKVs, N},
                     Keep = zlib:gzip(erlang:term_to_binary(Args)),
                     hibernate_scan(Keep);
                 false ->
-                    scan(BT1, BT2, Out, IsLastLevel, AKVs, BKVs, {0, none})
+                    scan(IXA, IXB, Out, IsLastLevel, AKVs, BKVs, {0, none})
             end
     end;
 
-scan(BT1, BT2, Out, IsLastLevel, [], BKVs, Step) ->
-    case hanoidb_reader:next_node(BT1) of
+scan(IXA, IXB, Out, IsLastLevel, [], BKVs, Step) ->
+    case hanoidb_reader:next_node(IXA) of
         {node, AKVs} ->
-            scan(BT1, BT2, Out, IsLastLevel, AKVs, BKVs, Step);
+            scan(IXA, IXB, Out, IsLastLevel, AKVs, BKVs, Step);
         end_of_data ->
-            hanoidb_reader:close(BT1),
-            scan_only(BT2, Out, IsLastLevel, BKVs, Step)
+            hanoidb_reader:close(IXA),
+            scan_only(IXB, Out, IsLastLevel, BKVs, Step)
     end;
 
-scan(BT1, BT2, Out, IsLastLevel, AKVs, [], Step) ->
-    case hanoidb_reader:next_node(BT2) of
+scan(IXA, IXB, Out, IsLastLevel, AKVs, [], Step) ->
+    case hanoidb_reader:next_node(IXB) of
         {node, BKVs} ->
-            scan(BT1, BT2, Out, IsLastLevel, AKVs, BKVs, Step);
+            scan(IXA, IXB, Out, IsLastLevel, AKVs, BKVs, Step);
         end_of_data ->
-            hanoidb_reader:close(BT2),
-            scan_only(BT1, Out, IsLastLevel, AKVs, Step)
+            hanoidb_reader:close(IXB),
+            scan_only(IXA, Out, IsLastLevel, AKVs, Step)
     end;
 
-scan(BT1, BT2, Out, IsLastLevel, [{Key1,Value1}|AT]=_AKVs, [{Key2,_Value2}|_BT]=BKVs, Step)
+scan(IXA, IXB, Out, IsLastLevel, [{Key1,Value1}|AT]=_AKVs, [{Key2,_Value2}|_IX]=BKVs, Step)
   when Key1 < Key2 ->
     Out3 =
         case ?LOCAL_WRITER of
@@ -144,8 +144,8 @@ scan(BT1, BT2, Out, IsLastLevel, [{Key1,Value1}|AT]=_AKVs, [{Key2,_Value2}|_BT]=
                 ok = hanoidb_writer:add(Out, Key1, Value1),
                 Out
         end,
-    scan(BT1, BT2, Out3, IsLastLevel, AT, BKVs, step(Step));
-scan(BT1, BT2, Out, IsLastLevel, [{Key1,_Value1}|_AT]=AKVs, [{Key2,Value2}|BT]=_BKVs, Step)
+    scan(IXA, IXB, Out3, IsLastLevel, AT, BKVs, step(Step));
+scan(IXA, IXB, Out, IsLastLevel, [{Key1,_Value1}|_AT]=AKVs, [{Key2,Value2}|IX]=_BKVs, Step)
   when Key1 > Key2 ->
     Out3 =
         case ?LOCAL_WRITER of
@@ -156,8 +156,8 @@ scan(BT1, BT2, Out, IsLastLevel, [{Key1,_Value1}|_AT]=AKVs, [{Key2,Value2}|BT]=_
                 ok = hanoidb_writer:add(Out, Key2, Value2),
                 Out
         end,
-    scan(BT1, BT2, Out3, IsLastLevel, AKVs, BT, step(Step));
-scan(BT1, BT2, Out, IsLastLevel, [{_Key1,_Value1}|AT]=_AKVs, [{Key2,Value2}|BT]=_BKVs, Step) ->
+    scan(IXA, IXB, Out3, IsLastLevel, AKVs, IX, step(Step));
+scan(IXA, IXB, Out, IsLastLevel, [{_Key1,_Value1}|AT]=_AKVs, [{Key2,Value2}|IX]=_BKVs, Step) ->
     Out3 =
         case ?LOCAL_WRITER of
             true ->
@@ -167,20 +167,20 @@ scan(BT1, BT2, Out, IsLastLevel, [{_Key1,_Value1}|AT]=_AKVs, [{Key2,Value2}|BT]=
                 ok = hanoidb_writer:add(Out, Key2, Value2),
                 Out
         end,
-    scan(BT1, BT2, Out3, IsLastLevel, AT, BT, step(Step, 2)).
+    scan(IXA, IXB, Out3, IsLastLevel, AT, IX, step(Step, 2)).
 
 hibernate_scan_only(Keep) ->
     erlang:garbage_collect(),
     receive
         {step, From, HowMany} ->
-            {BT, OutBin, IsLastLevel, KVs, N} = erlang:binary_to_term(zlib:gunzip(Keep)),
-            scan_only(hanoidb_reader:deserialize(BT),
+            {IX, OutBin, IsLastLevel, KVs, N} = erlang:binary_to_term(zlib:gunzip(Keep)),
+            scan_only(hanoidb_reader:deserialize(IX),
                       hanoidb_writer:deserialize(OutBin),
                       IsLastLevel, KVs, {N+HowMany, From})
     end.
 
 
-scan_only(BT, Out, IsLastLevel, KVs, {N, FromPID}) when N < 1, KVs =/= [] ->
+scan_only(IX, Out, IsLastLevel, KVs, {N, FromPID}) when N < 1, KVs =/= [] ->
     case FromPID of
         none ->
             ok;
@@ -190,18 +190,18 @@ scan_only(BT, Out, IsLastLevel, KVs, {N, FromPID}) when N < 1, KVs =/= [] ->
 
     receive
         {step, From, HowMany} ->
-            scan_only(BT, Out, IsLastLevel, KVs, {N+HowMany, From})
+            scan_only(IX, Out, IsLastLevel, KVs, {N+HowMany, From})
     after ?HIBERNATE_TIMEOUT ->
-            Args = {hanoidb_reader:serialize(BT),
+            Args = {hanoidb_reader:serialize(IX),
                     hanoidb_writer:serialize(Out), IsLastLevel, KVs, N},
             Keep = zlib:gzip(erlang:term_to_binary(Args)),
             hibernate_scan_only(Keep)
     end;
 
-scan_only(BT, Out, IsLastLevel, [], {_, FromPID}=Step) ->
-    case hanoidb_reader:next_node(BT) of
+scan_only(IX, Out, IsLastLevel, [], {_, FromPID}=Step) ->
+    case hanoidb_reader:next_node(IX) of
         {node, KVs} ->
-            scan_only(BT, Out, IsLastLevel, KVs, Step);
+            scan_only(IX, Out, IsLastLevel, KVs, Step);
         end_of_data ->
             case FromPID of
                 none ->
@@ -209,14 +209,14 @@ scan_only(BT, Out, IsLastLevel, [], {_, FromPID}=Step) ->
                 {PID, Ref} ->
                     PID ! {Ref, step_done}
             end,
-            hanoidb_reader:close(BT),
+            hanoidb_reader:close(IX),
             terminate(Out)
     end;
 
-scan_only(BT, Out, true, [{_,?TOMBSTONE}|Rest], Step) ->
-    scan_only(BT, Out, true, Rest, step(Step));
+scan_only(IX, Out, true, [{_,?TOMBSTONE}|Rest], Step) ->
+    scan_only(IX, Out, true, Rest, step(Step));
 
-scan_only(BT, Out, IsLastLevel, [{Key,Value}|Rest], Step) ->
+scan_only(IX, Out, IsLastLevel, [{Key,Value}|Rest], Step) ->
     Out3 =
         case ?LOCAL_WRITER of
             true ->
@@ -226,4 +226,4 @@ scan_only(BT, Out, IsLastLevel, [{Key,Value}|Rest], Step) ->
                 ok = hanoidb_writer:add(Out, Key, Value),
                 Out
         end,
-    scan_only(BT, Out3, IsLastLevel, Rest, step(Step)).
+    scan_only(IX, Out3, IsLastLevel, Rest, step(Step)).

commit 8026dcbdd42d4d3adce491d838d2a4881e4b9ea3

fix FoldingPIDs/NextList confusion which makes snapshot_range stuck
the bug was introduced by commit f324972cdd91e6f457d0a4dd11beac1a80332e7e.

src/hanoidb_level.erl
@@ -439,10 +439,10 @@ main_loop(State = #state{ next=Next }) ->
 
             ?log("init_range_fold ~p -> ~p", [Range, WorkerPID]),
 
-            {FoldingPIDs, NextList} =
+            {NextList, FoldingPIDs} =
             case {State#state.a, State#state.b, State#state.c} of
                 {undefined, undefined, undefined} ->
-                    {[], List};
+                    {List, []};
 
                 {_, undefined, undefined} ->
                     ok = file:make_link(filename("A", State), filename("AF", State)),